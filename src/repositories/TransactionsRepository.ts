e5dc6d330ad78c03b3207bdaee4d481e12eecc7d
Gabriel Roberto <gabrielrmsantos@outlook.com>
2020-07-31 01:09:05 -0300

    funcionalidades desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -15,14 +21,36 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const income = this.transactions.reduce((sumIncome, transaction) => {
+      return transaction.type === 'income'
+        ? sumIncome + transaction.value
+        : sumIncome;
+    }, 0);
+
+    const outcome = this.transactions.reduce((sumOutcome, transaction) => {
+      return transaction.type === 'outcome'
+        ? sumOutcome + transaction.value
+        : sumOutcome;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
